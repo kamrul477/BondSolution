13137c80f72273e011a9cd6fb6e9e73a8b67cd41
User <User@DESKTOP-6L4MMRG>
2017-03-30 17:55:47 +0600

    30.03.17

---
+++ Mvc.BondApp/Controllers/StockInfoController.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Mvc.BondApp.ViewModels;
+using System.Web.Mvc;
+
+namespace Mvc.BondApp.Controllers
+{
+    public class StockInfoController : Controller
+    {
+
+        #region COMMON AREA
+
+        private BondModel _context = new BondModel();
+
+
+        #endregion
+
+        #region STOCK ENTRY AREA
+
+        public ActionResult StockEntry()
+        {
+            ViewBag.BondCodeForStockEntry = _context.BONDINFOes.ToList();
+            var model = new StockEntryViewModel();
+            return View(model);
+        }
+
+
+        #endregion
+
+
+
+
+
+
+    }
+}
---
+++ Mvc.BondApp/ViewModels/StockEntryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Mvc.BondApp.ViewModels
+{
+    public class StockEntryViewModel
+    {
+        public string BondCode { get; set; }
+        public string IndentNo { get; set; }
+        public DateTime IndentDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public int TotalScript { get; set; }
+        public decimal ScriptValue { get; set; }
+        public string Prefix { get; set; }
+        public int StartingScriptNo { get; set; }
+        public int NoOfContinuousScript { get; set; }
+
+    }
+}
