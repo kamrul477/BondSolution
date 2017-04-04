2c5117c1a50c2f4c0a99feed9acc8f9b3b58c5fb
User <User@DESKTOP-6L4MMRG>
2017-04-04 17:56:52 +0600

    04.04.17

---
+++ Mvc.BondApp/Controllers/TransactionController.cs
@@ -0,0 +1,31 @@
+﻿using Mvc.BondApp.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Mvc.BondApp.Controllers
+{
+    public class TransactionController : Controller
+    {
+        BondModel _context = new BondModel();
+
+        #region INSTALLMENT PAYMENT AREA
+
+        #endregion
+        // GET: Transaction
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ApplicationSearch(string applicationNo)
+        {
+            var application = _context.BONDAPPLICATIONs.SingleOrDefault(a => a.BONDSCN.Equals(applicationNo));
+            var model = new InstallmentPaymentViewModel()
+            {
+                Bondapplication = application
+            };
+            
+            return View(model);
+        }
+    }
+}
